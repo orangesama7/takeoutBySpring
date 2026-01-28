56efde76139358861abcdb88c5717f9fbf63b20e
JinjiKikko77_ <2056229713@qq.com>
2026-01-28 17:35:27 +0800

    启用禁用员工

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -109,4 +109,21 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         return new PageResult(total,records);
     }
+
+
+    /**
+     * 启用或禁用员工账号
+     * @param status
+     * @param id
+     */
+    public void startOrStop(Integer status, Long id) {
+
+        Employee employee = new Employee();
+        employee.setStatus(status);
+        employee.setId(id);
+        //另一种写法
+        //Employee employee = Employee.builder().id(id).status(status).build();
+
+        employeeMapper.update(employee);
+    }
 }
